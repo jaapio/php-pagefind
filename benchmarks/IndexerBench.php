fd86752cd6634b523963a5ed0669a0a5a43d66eb
Jaapio <jaap@ijaap.nl>
2025-07-29 22:59:49 +0200

    Add more benchmarks

--- benchmarks/IndexerBench.php
+++ benchmarks/IndexerBench.php
@@ -13,6 +13,7 @@ class IndexerBench
     private string $sampleDir;
     private ServiceConfig $config;
     private string $outputDir;
+    private array $sampleContent = [];
 
     public function setUp(): void
     {
@@ -24,6 +25,23 @@ class IndexerBench
         }
 
         $this->config = new ServiceConfig(false, false);
+
+        // Cache sample content to avoid reading from disk during benchmarks
+        $this->sampleContent = [
+            'article1' => file_get_contents($this->sampleDir . '/article1.html'),
+            'article2' => file_get_contents($this->sampleDir . '/article2.html'),
+            'article3' => file_get_contents($this->sampleDir . '/article3.html')
+        ];
+    }
+
+    /**
+     * @Revs(5)
+     * @Iterations(5)
+     */
+    public function benchIndexEmpty(): void
+    {
+        $indexer = new Indexer($this->config);
+        $files = $indexer->writeFiles($this->outputDir);
     }
 
     /**
@@ -37,7 +55,7 @@ class IndexerBench
         $indexer->addHtmlFile(
             'article1',
             '/article1',
-            file_get_contents($this->sampleDir . '/article1.html')
+            $this->sampleContent['article1']
         );
 
         $files = $indexer->getFiles();
@@ -65,7 +83,7 @@ class IndexerBench
         $indexer->addHtmlFile(
             'article1',
             '/article1',
-            file_get_contents($this->sampleDir . '/article1.html')
+            $this->sampleContent['article1']
         );
 
         $indexer->writeFiles($this->outputDir);
@@ -82,19 +100,19 @@ class IndexerBench
         $indexer->addHtmlFile(
             'article1',
             '/article1',
-            file_get_contents($this->sampleDir . '/article1.html')
+            $this->sampleContent['article1']
         );
 
         $indexer->addHtmlFile(
             'article2',
             '/article2',
-            file_get_contents($this->sampleDir . '/article2.html')
+            $this->sampleContent['article2']
         );
 
         $indexer->addHtmlFile(
             'article3',
             '/article3',
-            file_get_contents($this->sampleDir . '/article3.html')
+            $this->sampleContent['article3']
         );
 
         $files = $indexer->getFiles();
@@ -122,19 +140,19 @@ class IndexerBench
         $indexer->addHtmlFile(
             'article1',
             '/article1',
-            file_get_contents($this->sampleDir . '/article1.html')
+            $this->sampleContent['article1']
         );
 
         $indexer->addHtmlFile(
             'article2',
             '/article2',
-            file_get_contents($this->sampleDir . '/article2.html')
+            $this->sampleContent['article2']
         );
 
         $indexer->addHtmlFile(
             'article3',
             '/article3',
-            file_get_contents($this->sampleDir . '/article3.html')
+            $this->sampleContent['article3']
         );
 
         $indexer->writeFiles($this->outputDir);
@@ -175,5 +193,44 @@ class IndexerBench
         $indexer->addDirectory($this->sampleDir);
 
         $indexer->writeFiles($this->outputDir);
+    }
+
+    /**
+     * Benchmark the impact of increasing file count
+     *
+     * @Revs(1)
+     * @Iterations(1)
+     * @ParamProviders("provideFileCounts")
+     */
+    public function benchScalingFileCount(array $params): void
+    {
+        $fileCount = $params['count'];
+        $indexer = new Indexer($this->config);
+
+        // Generate and add the specified number of files
+        for ($i = 0; $i < $fileCount; $i++) {
+            // Rotate through our sample content to create varied content
+            $contentKey = array_keys($this->sampleContent)[$i % count($this->sampleContent)];
+
+            $indexer->addHtmlFile(
+                "page-{$i}",
+                "/page-{$i}",
+                $this->sampleContent[$contentKey]
+            );
+        }
+
+        $indexer->writeFiles($this->outputDir);
+    }
+
+    /**
+     * Provides parameters for scaling benchmarks
+     */
+    public function provideFileCounts(): \Generator
+    {
+        yield 'files-10' => ['count' => 10];
+        yield 'files-25' => ['count' => 25];
+        yield 'files-50' => ['count' => 50];
+        yield 'files-100' => ['count' => 100];
+        yield 'files-10000' => ['count' => 10000];
     }
 }
